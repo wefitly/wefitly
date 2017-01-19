1cd567d6a229d56e8b9dbdf997b0b2fcb0757265
Brendan Mok <bmok88@gmail.com>
2017-01-19 11:51:34 -0800

    Create trainertable and row components

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,39 @@
 import React from 'react';
 import {render} from 'react-dom';
+var trainerData = require('./trainers');
 
 class App extends React.Component {
   render () {
-        return <p> Hello React!</p>;
-
+    return (
+      <TrainerTable trainers={trainerData}/>
+    )
   }
-
 }
 
-render(<App/>, document.getElementById('app'));
+var TrainerTable = ({trainers}) => (
+  <table>
+    <tbody>
+    {trainers.map((trainer, i) =>
+        <TrainerRow firstname={trainer.firstname} lastname={trainer.lastname} key={i}/>
+    )}
+    </tbody>
+  </table>
+);
+
+class TrainerRow extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <tr>
+        <td className="trainer-name">{this.props.firstname + ' ' + this.props.lastname}</td>
+      </tr>
+    )
+  }
+}
+
+render(<App trainer={trainerData}/>, document.getElementById('app'));
 //this is a comment
+window.App = App;
