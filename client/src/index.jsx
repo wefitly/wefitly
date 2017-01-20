45c024a584a460ff66be980849d2f32d87e3215c
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-01-19 18:14:15 -0800

    add trainer signup client route

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,6 +4,7 @@ import {render} from 'react-dom';
 import {Router, Route, Link, hashHistory} from 'react-router';
 import Home from './home.jsx'
 import USignUp from './user-signup.jsx'
+import TrainerSignup from './trainerSignup.jsx'
 
 
 class App extends React.Component {
@@ -13,6 +14,7 @@ class App extends React.Component {
 
       <Router history={hashHistory}>
         <Route path="/signup" component={USignUp}/>
+        <Route path="/trainersignup" component={TrainerSignup}/>
         <Route path="/" component={Home}/>
       </Router>
     );
