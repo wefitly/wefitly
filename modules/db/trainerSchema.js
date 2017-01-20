b6c0f9dc3780d6d98ac5b26f1bc1a09fc9f384a9
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-19 19:04:44 -0800

    adds a hashpasword function(that probably doesn't work) to the trainerSchema file

--- modules/db/trainerSchema.js
+++ modules/db/trainerSchema.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose')
-
 var Schema = mongoose.Schema
+var bcrypt = require('bcrypt-nodejs')
+var SALT_WORK_FACTOR = 10;
 
 var TrainerSchema = new Schema({
   username: String,
   password: String,
+  salt: String,
   firstname: String,
   lastname: String,
   profilepic: String,
@@ -15,5 +17,24 @@ var TrainerSchema = new Schema({
   introduction: String
 })
 
-module.exports = TrainerSchema;
+TrainerSchema.methods.hashPassword = function(){
+  var trainer = this;
+  return new Promise(function(resolve, reject){
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+      if(err){
+        console.error(error)
+      }
+      bcrypt.hash(trainer.password, salt, null, function(err, hash){
+        if(err){
+          console.error(error)
+        }
+        trainer.password = hash;
+        trainer.salt = salt;
+      })
+    })
+  })
+}
+
+
+module.exports = mongoose.method('Trainer', TrainerSchema)
 
