02918d12d91fc0c62e4946337f4b9e6852a8dacd
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-01-24 13:28:12 -0800

    trainer profile change

--- client/src/trainerProfile.jsx
+++ client/src/trainerProfile.jsx
@@ -6,25 +6,25 @@ class Signup extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      profilePicture : this.props.profilePicture
-    }
 
+    }
+    this.handleFormChange = this.handleFormChange.bind(this);
+
+  }
+
+  handleFormChange(e){
+    this.setState($(e.target).name, $(e.target).val());
   }
 
   onSubmit(e) {
     e.preventDefault();
+    const data = $(e.target).serializeArray().
 
     $.ajax({
-      url         : this.props.endpoint,
+      url         : '/api/trainerprofile',
       type        : 'POST',
       ContentType :'application/json',
-      data        : {
-        //we will use session ID to know which entry to apply the bio to. 
-        //so we will use the value stored in the session to find the trainer,
-        //and then apply the bio to the trainer
-        'firstname':this.refs.firstname.value,
-        'lastname' :this.refs.lastname.value,
-      }
+      data        : this.state 
     }).done(function(response){
       console.log('profile data recieved');
     }).fail(function(response){
@@ -37,15 +37,15 @@ class Signup extends React.Component {
       <div id='signup'>
         <form onSubmit={this.onSubmit.bind(this)}>
           <span>Profile</span>
-          <input type='text' name='firstname'  placeholder='First Name'>{this.props.firstname}</input><br/>
-          <input type='text' name='lastname' placeholder='Last Name'>{this.props.lastname}</input><br/>
-          <input type='text' name='bio' required ref='bio' placeholder='Tell us about yourself'>{this.props.bio || ''}</input><br/>
+          <input onChange={handleFormChange} type='text' name='firstname'  placeholder='First Name'>{this.props.firstname}</input><br/>
+          <input onChange={handleFormChange} type='text' name='lastname' placeholder='Last Name'>{this.props.lastname}</input><br/>
+          <input onChange={handleFormChange} type='text' name='bio'  placeholder='Tell us about yourself'>{this.props.bio || ''}</input><br/>
           <p>Services Offered:</p><br/>
-          <input type='radio' name='1on1' value='1 on 1 personal training'/> 1 on 1 personal training <br/>
-          <input type='radio' name='dietcons' value='Diet consulting'/> Diet consulting <br/>
-          <input type='radio' name='group' value='Group Training'/>Group training<br/>
-          <input type='radio' name='remote' value='Remote Training'/>Remote Training<br/>
-          <input type='text' name='rate' placeholder='hourly rate'></input><br/>
+          <input onChange={handleFormChange} type='radio' name='1on1' value='1 on 1 personal training'/> 1 on 1 personal training <br/>
+          <input onChange={handleFormChange} type='radio' name='dietcons' value='Diet consulting'/> Diet consulting <br/>
+          <input onChange={handleFormChange} type='radio' name='group' value='Group Training'/>Group training<br/>
+          <input onChange={handleFormChange} type='radio' name='remote' value='Remote Training'/>Remote Training<br/>
+          <input onChange={handleFormChange} type='text' name='rate' placeholder='hourly rate'></input><br/>
           <input type='submit' value='Submit' /><br/>
         </form>
       </div>
