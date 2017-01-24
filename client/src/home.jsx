a52683d48d83d9f4a26c721426ed2fa2a3adf7a7
Sawyer Schumacher <sawyer.schumacher@gmail.com>
2017-01-24 14:15:45 -0800

    add profile editor

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -7,7 +7,7 @@ class Home extends React.Component {
         <a href="#/signup">Go to user signup</a><br/>
         <a href="#/trainersignup">Go to trainer signup</a><br/>
         <a href="#/trainersignin">Go to trainer sign in</a><br/>
-        <a href="#/usersignup">Go to user signup</a>
+        <a href="#/usersignup">Go to user signup</a><br/>
         <a href="#/trainerprofile">Trainer Profile editor (not finished)</a><br/>
       </div>
     );
