40304d83fa4b97b498ec2c6f26213e6e7f2f7c75
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-25 14:34:14 -0800

    defines a handleFilterChange method, sends ajax get request

--- client/src/trainerTable.jsx
+++ client/src/trainerTable.jsx
@@ -1,23 +1,40 @@
 import React from 'react';
-import TrainerRow from './tableRow'
+import FilterBar from './filterBar.jsx'
+import $ from 'jquery'
+// import TrainerRow from './tableRow'
 class TrainerTable extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      showCount:10,
-      zipCode:80005
+      entries: [],
     }
   }
 
-  getAllTrainers(){
-    //get data
-    // for each data, draw a table row
+  getAllTrainers() {
+
   }
 
+  handleFilterChange(e){
+    const $ele = $(e.target).val();
+    console.log(typeof $ele)
+    $.ajax({
+      url: this.props.endpoint,
+      method: 'GET',
+      ContentType :'application/json',
+      data: {
+        location: $ele,
+      },
+    })
+    .done( (response) => {
+      console.log('handleFilterChange-----');
+    })
+    .fail(function(response){
+      console.log('signup data transmission failure');
+    });
+  }
   render(){
-
-
-  }
+    return <FilterBar handleChange={this.handleFilterChange.bind(this)}/>
+  };
 }
 
-export default TableRow;
+export default TrainerTable;
