a804899b90d44d252e6e5b370fcfc39f703ef309
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-27 13:22:54 -0800

    imports passwords, uses them for signup and signin

--- modules/db/userMethods.js
+++ modules/db/userMethods.js
@@ -2,60 +2,20 @@ const UserSchema = require('./userSchema.js').UserSchema;
 const UserModel = require('./userSchema.js').UserModel;
 const bcrypt = require('bcrypt-nodejs');
 const Q = require('q');
+const passwordHelpers = require('./passwordHelpers.js');
 
 const SALT_WORK_FACTOR = 10;
 const findUser = Q.nbind(UserModel.findOne, UserModel);
 const findAll = Q.nbind(UserModel.find, UserModel);
 
 
-UserSchema.pre('save', function(next){
-  const newUser = this;
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-      return console.error(err);
-    }
-    bcrypt.hash(newUser.password, salt, null, (error, hash) => {
-      if (error) {
-        return console.error(error);
-      }
-      newUser.password = hash;
-      newUser.salt = salt;
-      next();
-    });
-  });
-});
-
-UserModel.signup = function(user, next){
-  
- var newUser = new UserModel({
-    firstname: user.firstname,
-    lastname: user.lastname,
-    email: user.email,
-    password: user.password
-  }).save(function(err) {
-    if (err) throw err;
-  });
+UserModel.signup = (user, next) => {
+ passwordHelpers.signup(user, UserModel, next);
 }
 
-
 UserModel.comparePassword = (email, candidatePassword, next) => {
-  findUser({ email: email })
-  .then((match) => {
-    if (match) {
-      bcrypt.compare(candidatePassword, match.password, (err, isMatch) => {
-        if (err) {
-          next(err, null);
-        }
-        console.log(isMatch);
-        next(null, isMatch);
-      });
-    } else {
-      console.log('email was not found');
-      next('email not found');
-    }
-  });
-};
-
+  passwordHelpers.comparePassword(email, candidatePassword, findUser, next);
+}
 
 UserModel.findAllTrainers = (next) => {
   findAll({})
