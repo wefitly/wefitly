db54458e22ebc6fb658fb2c9a99ded8605256bf2
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-27 14:51:48 -0800

    refactors user and trainer middleware to own file, calls them on get and post to apis

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const passport = require('./modules/auth.js');
 const cookieParser = require('cookie-parser');
 const TrainerModel = require('./modules/db/trainerMethods.js');
 const UserModel = require('./modules/db/userMethods.js');
+const UserController = require('./modules/db/UserController.js');
+const TrainerController = require('./modules/db/TrainerController.js');
 
 mongoose.connect('mongodb://localhost:27017/wefitlytest');
 
@@ -33,111 +35,14 @@ app.use(session({
 // app.use(passport.initialize());
 // app.use(passport.session());
 
-//routes should be in their own file, refactor later
-app.post('/api/userSignup', function(req, res) {
-  const user = req.body;
-  UserModel.signup(user, function() {
-  });
 
-  res.end('success');
-});
-
-app.post('/api/userSignin', function(req, res) {
-  const password = req.body.password;
-  const email = req.body.email;
-  UserModel.comparePassword(email, password, (err, isMatch) => {
-    if (err) {
-      res.end(err);
-    }
-    if (isMatch) {
-      req.session.email = email;
-      req.session.save();
-      res.end('success');
-    } else {
-      res.end('failed');
-    }
-  });
-});
-
-// routes should be in their own file, refactor later
-app.post('/api/trainerSignup', (req, res) => {
-  const user = req.body;
-  TrainerModel.signup(user, (err) => {
-    if (err){
-      console.log('failed')
-      res.end('fail')
-    }else {
-      req.session.isTrainer = true;
-      req.session.email = user.email;
-      req.session.save();
-      res.end('success');
-    }
-  });
-
-});
-
-
-app.post('/api/trainerSignin', (req, res) => {
-  const password = req.body.password;
-  const email = req.body.email;
-  TrainerModel.comparePassword(email, password, (err, isMatch) => {
-    if (err) {
-      res.end(err);
-    }
-    if (isMatch) {
-      req.session.isTrainer = true;
-      req.session.email = email;
-      req.session.save();
-      res.end('success');
-    } else {
-      res.sendStatus(504);
-    }
-  });
-});
-
-app.get('/api/filterTrainers', (req, res) => {
-  const location = req.query.location;
-  TrainerModel.filterTrainers(location, (results) => {
-    res.json(results);
-  })
-});
-
-app.get('/api/getAllTrainers', (req, res) => {
-  TrainerModel.findAllTrainers((results) => {
-    res.json(results);
-  });
-});
-
-app.post('/api/updateTrainer',(req,res)=>{
-  if (req.session){
-    if (req.session.isTrainer){
-      const updateObj = {
-        firstname:req.body.firstname,
-        lastname:req.body.lastname,
-        bio:req.body.bio,
-        services:{
-          '1on1':req.body.oneonone?true:false,
-          'dietcons':req.body.dietcons?true:false,
-          'group':req.body.group?true:false,
-          'remote':req.body.remote?true:false,
-        },
-        rate:req.body.rate
-      }
-      TrainerModel.updateTrainer(req.session.email,updateObj,(err)=>{
-        if (err){
-          res.sendStatus(504);
-        }else{
-          res.end('success');
-        }
-      })
-    }else{
-      res.sendStatus(401);
-    }
-  }else{
-    console.log('unauth on profile');
-    res.sendStatus(401);
-  }
-})
+app.post('/api/userSignup', UserController.signup);
+app.post('/api/userSignin', UserController.signin);
+app.post('/api/trainerSignup', TrainerController.signup);
+app.post('/api/trainerSignin', TrainerController.signin);
+app.get('/api/filterTrainers', TrainerController.filter);
+app.get('/api/getAllTrainers', TrainerController.getAll);
+app.post('/api/updateTrainer', TrainerController.updateTrainer)
 
 
 // mongoose.connection('mongodb://localhost/')
