541e3bf5122d532a1db8cb6ec944d63b843dbf2a
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-26 17:48:32 -0800

    adds TrainerTable component to UserDash

--- client/src/UserDash.jsx
+++ client/src/UserDash.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import TrainerTable from './trainerTable.jsx';
 //This is just a placeholder for the userdashboard. Signing in/up for a user should redirect them to the trainer table componenet.
 const UserDash = (props) => (
   <div>
     <h1>Your Dashboard</h1>
+    <TrainerTable />
   </div>
 );
 
