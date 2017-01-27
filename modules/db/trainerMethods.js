4867ce8d418db4b6bbf1c472c1fe32278d5e853e
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-27 11:53:12 -0800

    imports password helpers, uses those in signup and compare functions

--- modules/db/trainerMethods.js
+++ modules/db/trainerMethods.js
@@ -1,62 +1,23 @@
 const TrainerSchema = require('./TrainerSchema.js').TrainerSchema;
 const TrainerModel = require('./TrainerSchema.js').TrainerModel;
-const bcrypt = require('bcrypt-nodejs');
 const Q = require('q');
+const passwordHelpers = require('./passwordHelpers.js')
 
-const SALT_WORK_FACTOR = 10;
-const findUser = Q.nbind(TrainerModel.findOne, TrainerModel);
-const findAll = Q.nbind(TrainerModel.find, TrainerModel);
+const findTrainer = Q.nbind(TrainerModel.findOne, TrainerModel);
+const findAllTrainers = Q.nbind(TrainerModel.find, TrainerModel);
 
 
-TrainerSchema.pre('save', function (next) {
-  let newTrainer = this;
-  bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-    if (err) {
-      return console.error(err);
-    }
-    bcrypt.hash(newTrainer.password, salt, null, (error, hash) => {
-      if (error) {
-        return console.error(error);
-      }
-      newTrainer.password = hash;
-      newTrainer.salt = salt;
-      next();
-    });
-  });
-});
 
 TrainerModel.signup = (user, next) => {
-  console.log(user);
-  let newTrainer = new TrainerModel({
-    username: user.email,
-    password: user.password,
-    location: user.location,
-  })
-    .save((err)=>{
-      next(err);
-    });
-};
+  passwordHelpers.signup(user, TrainerModel, next);
+}
 
-
-TrainerModel.comparePassword = (email, candidatePassword, next) => {
-  findUser({ username: email })
-  .then((match) => {
-    if (match) {
-      bcrypt.compare(candidatePassword, match.password, (err, isMatch) => {
-        if (err) {
-          next(err, null);
-        }
-        next(null, isMatch);
-      });
-    } else {
-      next('email not found');
-    }
-  });
-};
-
+TrainerModel.comparePassword = (email, candidatePassword, next) =>{
+  passwordHelpers.comparePassword(email, candidatePassword, findTrainer, next);
+}
 
 TrainerModel.findAllTrainers = (next) => {
-  findAll({})
+  findAllTrainers({})
   .then((results) => {
     return next(results);
   })
@@ -65,9 +26,8 @@ TrainerModel.findAllTrainers = (next) => {
   });
 };
 
-// refactor to filter by location
 TrainerModel.filterTrainers = (location, next) => {
-  findAll({ location: location }, { username: 1, location: 1 } )
+  findAllTrainers({ location: location }, { username: 1, location: 1, services: 1 } )
   .then((results) => {
     return next(results);
   })
