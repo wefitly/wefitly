1c36000e6a18dfe16f01bf5de3353c24607389d2
colbisaurusrex <c.barthelmess1@gmail.com>
2017-01-30 12:23:27 -0800

    changes props reference for class based component

--- client/src/tableRow.jsx
+++ client/src/tableRow.jsx
@@ -34,8 +34,8 @@ class TableRow extends React.Component {
             </div>
             <div className="services-container">
               <ul className="services-list w-list-unstyled">
-                <li className="services-list-item">{props.firstName} {props.lastName}</li>
-                <li className="services-list-item">Location:{props.location}</li>
+                <li className="services-list-item">{this.props.firstName} {this.props.lastName}</li>
+                <li className="services-list-item">Location:{this.props.location}</li>
               </ul>
             </div>
           </div>
