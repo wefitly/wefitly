aa9804af80f1f740ab34236e2f24d86993bd17ba
terry <closer@gmail.com>
2017-01-30 18:59:14 -0800

    Fix filtering

--- client/src/filterBar.jsx
+++ client/src/filterBar.jsx
@@ -4,9 +4,9 @@ var FilterBar = (props) => (
       <form data-name="Email Form" id="email-form" name="email-form">
         <select className="w-select" id="field" name="field" onChange={props.handleChange}>
           <option value="">Select one...</option>
-          <option value="First">San Francisco</option>
-          <option value="Second">San Mateo</option>
-          <option value="Third">Pleasanton</option>
+          <option value="San Francisco">San Francisco</option>
+          <option value="San Mateo">San Mateo</option>
+          <option value="Pleasanton">Pleasanton</option>
         </select>
       </form>
 );
